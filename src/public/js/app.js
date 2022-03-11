4e28ab10a49a318f410511de92832775b3d4b335
GanghyeonSeo <sgh000109@gmail.com>
2022-03-11 18:18:27 +0900

    nickname form 추가, JSON.stringify를 통한 데이터 전송

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,13 +1,23 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nicknameForm = document.querySelector("#nickname");
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+
+  return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
   console.log("Connected to Server");
 });
 
 socket.addEventListener("message", (message) => {
-  console.log("New message: ", message.data, "from the server");
+  const li = document.createElement("li");
+
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
@@ -16,9 +26,20 @@ socket.addEventListener("close", () => {
 
 function handleSubmit(event) {
   event.preventDefault();
+
   const input = messageForm.querySelector("input");
-  socket.send(input.value);
+
+  socket.send(makeMessage("new_message", input.value));
   input.value = "";
 }
 
+function handleNicknameSubmit(event) {
+  event.preventDefault();
+
+  const input = nicknameForm.querySelector("input");
+
+  socket.send(makeMessage("nickname", input.value));
+}
+
 messageForm.addEventListener("submit", handleSubmit);
+nicknameForm.addEventListener("submit", handleNicknameSubmit);
